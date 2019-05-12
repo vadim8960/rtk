1d3f52b2fd27cdb772d2daf0e08d6856b729dba2
Vadim <akimovvadim779@gmail.com>
2019-05-12 03:16:35 +0300

    Refactoring

--- pc/client_video.py
+++ pc/client_video.py
@@ -25,7 +25,12 @@ def video_thread():
 	sock_video = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock_video.connect((TCP_IP, TCP_PORT_VIDEO))
 
-	kp = 1
+	time.sleep(5)
+
+	sock_local = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock_local.connect((TCP_IP_LOCAL, 1924))
+
+	kp = 1.5
 	ki = 0
 	kd = 0
 	i = 0
@@ -38,31 +43,47 @@ def video_thread():
 		data = numpy.fromstring(stringData, dtype='uint8')
 		image = cv2.imdecode(data,1)
 		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-		cv2.imshow('Original', image)
+		# blur = cv2.GaussianBlur(gray, (5, 5), 0)
+		ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 		cv2.imshow('Bin', binary)
-
 		dimen = binary.shape
 		left  = 0
 		right = 0
-		it  = 0
-		while (binary[50,it] > 0 and it < dimen[1] - 1):
-			left += 1
-			it += 1
+		start_left = 150
+		end_left = 250
+		start_right = 390
+		end_right = 490
+		# while (binary[300, it] > 0 and it < dimen[1] - 200):
+		# 	left += 1
+		# 	it += 1
 
-		it = dimen[1] - 1
-		while (binary[50,it] > 0 and it > 0):
-			right += 1
-			it -= 1
+		# it = dimen[1] - 200
+		# while (binary[300, it] > 0 and it > 200):
+		# 	right += 1
+		# 	it -= 1
 
-		error = left - right
+		for it in range(start_left, end_left):
+			if (binary[410, it] == 0):
+				left += 1
+
+		for it in range(start_right, end_right):
+			if (binary[410, it] == 0):
+				right += 1
+
+		error = (left - right)
+
+		cv2.line(image, (start_left, 410), (end_left, 410), (255, 0, 0), 3)
+		cv2.line(image, (start_right, 410), (end_right, 410), (255, 0, 0), 3)
+		cv2.putText(image, str(left), (140, 410), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+		cv2.putText(image, str(error), (260, 410), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+		cv2.putText(image, str(right), (480, 410), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
+		cv2.imshow('Original', image)
 
 		p = kp * error
 		i += (ki * error)
 		d = kd * (error - error_old)
 		u = int(p + i + d)
-
-		sock_video.send(bytes([u]))
+		sock_local.send((str(u) + '\n').encode())
 
 		cv2.waitKey(1)
 
--- pc/run_client.sh
+++ pc/run_client.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+pkill -9 python
+
 sudo python3 client_gamepad.py &
 python3 client_video.py 
